bed58b9940354577d26ae1230385a50556c50464
Jarunga <president.sute@gmail.com>
2023-10-28 22:36:29 +0900

    dirの修正

--- src/reg/domain.py
+++ src/reg/domain.py
@@ -1,4 +1,4 @@
-from .extra import url_regex_custom
+from .extra.url_regex_custom import regex
 
 EMAIL_HEAD=['E-mail.','E-MAIL','Email.','MAIL.','Mail.','mailto:','-mail']
 
@@ -8,7 +8,7 @@ EMAIL_REMOVE_START=['//','abc','mailto:']
 '''RFC 5322 Official Standard_https://emailregex.com/'''
 EMAIL_REGEX=r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'''
 
-URL_REGEX_COMPILED=url_regex_custom.regex
+URL_REGEX_COMPILED=regex
 
 COMMON_DOMAIN=['yahoo.co.jp','gmail.com','softbank.jp','docomo.ne.jp','jcom.home.ne.jp','dion.ne.jp','hotmail.com','nifty.com','outlook.jp','ocn.ne.jp']
 
